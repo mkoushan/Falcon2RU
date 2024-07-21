f77305ac3331326eb041a99efb46f28aed2d9f23
mkoushan <mkoushan@macaw.me>
2024-07-21 10:23:40 +0430

    feat: space current costs

--- include/space_current.hpp
+++ include/space_current.hpp
@@ -11,6 +11,8 @@ public:
    Space_Current(const std::vector<Object*>& neighbors, const Object*& target);
    char show() const override{return '1';}
    const Object* targetCell(const Object* const entry) const override {return this->target;}
+   const int getEnergyCost() const override {return 2;}
+   const int getTimeCost() const override {return 1;}
 };
 
 #endif // SPACE_CURRENT_HPP_INCLUDED
