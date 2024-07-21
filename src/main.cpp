e29f187c5bc9fd8674c15fdaf7ab623d5ae8abc7
mkoushan <mkoushan@macaw.me>
2024-07-21 11:10:41 +0430

    chore: change main.cpp for debug purpose

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,9 @@
 #include <iostream>
+#include "object.hpp"
+#include "space_current.hpp"
+#include "space_object.hpp"
+#include "pipe.hpp"
+#include "wormhole.hpp"
 
 int main() {
   std::cout << "Hello World!\n";
